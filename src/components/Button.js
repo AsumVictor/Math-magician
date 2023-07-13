e42da6cfe0e8cbcc3780a728005a26a94b2a3374
AsumVictor <iamasum369@gmail.com>
2023-07-12 21:00:11 -0700

    Add propTYpes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
-function Button({value, classextend}) {
+function Button({ value, classextend }) {
   return (
-    <div className={`border flex items-center justify-center border-white bg-slate-200 text-black font-bold text-xl ${classextend} `}>
-{value}
+    <div
+      className={`border flex items-center justify-center border-white bg-slate-200 text-black font-bold text-xl ${classextend} `}
+    >
+      {value}
     </div>
   );
 }
+
+Button.propTypes = {
+  value: PropTypes.string.isRequired,
+  classextend: PropTypes.string,
+};
+
+Button.defaultProps = {
+  classextend: null,
+};
 
 export default Button;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,27 +4,27 @@ import Button from './Button';
 
 function Calculator() {
   return (
-    <div className='w-[9cm] md:w-[12cm] h-[9cm] md:h-[12cm] border m-10 grid grid-cols-4 grid-rows-6'>
+    <div className="w-[9cm] md:w-[12cm] h-[9cm] md:h-[12cm] border m-10 grid grid-cols-4 grid-rows-6">
       <Screen />
-      <Button value={'AC'}/>
-      <Button value={'+/-'}/>
-      <Button value={'%'}/>
-      <Button value={'÷'}classextend={'bg-orange-500'} />
-      <Button value={'7'}/>
-      <Button value={'8'}/>
-      <Button value={'9'}/>
-      <Button value={'×'}classextend={'bg-orange-500'} />
-      <Button value={'4'}/>
-      <Button value={'5'}/>
-      <Button value={'6'}/>
-      <Button value={'-'}classextend={'bg-orange-500'} />
-      <Button value={'1'}/>
-      <Button value={'2'}/>
-      <Button value={'3'}/>
-      <Button value={'+'}classextend={'bg-orange-500'} />
-      <Button value={'0'}classextend={'col-span-2'}/>
-      <Button value={'.'}/>
-      <Button value={'='}classextend={'bg-orange-500'} />
+      <Button value="AC" />
+      <Button value="+/-" />
+      <Button value="%" />
+      <Button value="÷" classextend="bg-orange-500" />
+      <Button value="7" />
+      <Button value="8" />
+      <Button value="9" />
+      <Button value="×" classextend="bg-orange-500" />
+      <Button value="4" />
+      <Button value="5" />
+      <Button value="6" />
+      <Button value="-" classextend="bg-orange-500" />
+      <Button value="1" />
+      <Button value="2" />
+      <Button value="3" />
+      <Button value="+" classextend="bg-orange-500" />
+      <Button value="0" classextend="col-span-2" />
+      <Button value="." />
+      <Button value="=" classextend="bg-orange-500" />
     </div>
   );
 }
