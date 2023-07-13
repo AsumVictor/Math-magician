e6ef2a4c79347864fb63802dda34430e9cc54f01
AsumVictor <iamasum369@gmail.com>
2023-07-12 20:42:47 -0700

    Add buton components

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Button({value, classextend}) {
   return (
-    <div className={`border flex items-center justify-center border-white bg-slate-200 text-white ${classextend} `}>
+    <div className={`border flex items-center justify-center border-white bg-slate-200 text-black font-bold text-xl ${classextend} `}>
 {value}
     </div>
   );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,25 +6,25 @@ function Calculator() {
   return (
     <div className='w-[9cm] md:w-[12cm] h-[9cm] md:h-[12cm] border m-10 grid grid-cols-4 grid-rows-6'>
       <Screen />
-      <Button value={5}/>
-      <Button value={5}/>
-      <Button value={5}/>
-      <Button value={5}classextend={'bg-orange-500'} />
-      <Button value={5}/>
-      <Button value={5}/>
-      <Button value={5}/>
-      <Button value={5}classextend={'bg-orange-500'} />
-      <Button value={5}/>
-      <Button value={5}/>
-      <Button value={5}/>
-      <Button value={5}classextend={'bg-orange-500'} />
-      <Button value={5}/>
-      <Button value={5}/>
-      <Button value={5}/>
-      <Button value={5}classextend={'bg-orange-500'} />
-      <Button value={5}classextend={'col-span-2'}/>
-      <Button value={5}/>
-      <Button value={5}classextend={'bg-orange-500'} />
+      <Button value={'AC'}/>
+      <Button value={'+/-'}/>
+      <Button value={'%'}/>
+      <Button value={'÷'}classextend={'bg-orange-500'} />
+      <Button value={'7'}/>
+      <Button value={'8'}/>
+      <Button value={'9'}/>
+      <Button value={'×'}classextend={'bg-orange-500'} />
+      <Button value={'4'}/>
+      <Button value={'5'}/>
+      <Button value={'6'}/>
+      <Button value={'-'}classextend={'bg-orange-500'} />
+      <Button value={'1'}/>
+      <Button value={'2'}/>
+      <Button value={'3'}/>
+      <Button value={'+'}classextend={'bg-orange-500'} />
+      <Button value={'0'}classextend={'col-span-2'}/>
+      <Button value={'.'}/>
+      <Button value={'='}classextend={'bg-orange-500'} />
     </div>
   );
 }
