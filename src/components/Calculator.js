d09a09fba9c98914b7d8c3186163cdb97af6118e
AsumVictor <iamasum369@gmail.com>
2023-07-12 19:55:51 -0700

    Add components

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Screen from "./Screen";
+import Button from "./Button";
+
+function Calculator() {
+  return (
+    <div>
+      <Screen />
+      <Button />
+    </div>
+  );
+}
+
+export default Calculator;
---
+++ src/components/Screen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Screen() {
+  return (
+    <div>Screen</div>
+  )
+}
+
+export default Screen
